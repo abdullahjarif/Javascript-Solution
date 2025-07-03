8d8365c5f989b717993fd98c2bf6783d28c95341
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-03 09:16:10 +0600

    Add files via upload

---
+++ Lecture 04 (JS Hoisting)/script.js
@@ -0,0 +1,29 @@
+// js declaration use hoisting
+
+// var x
+// let x
+// const x = 10
+// x = 10 // assign value
+// console.log(x);
+
+
+// hoisting
+// var x
+// x = 10
+// console.log(x)
+
+function abc(){
+    x = 50
+    console.log(x)
+    // var x
+    let x
+}
+
+abc()
+
+
+
+
+
+
+
