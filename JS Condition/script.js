294930488c9cdbdf9cbd85a441611d244ef3565a
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-03 10:28:39 +0600

    Add files via upload

---
+++ JS Condition/script.js
@@ -0,0 +1,43 @@
+// condition
+
+// let age = prompt("Please enter your age:");
+
+// sytex if condition if(condition){statement}
+// mathematical way age > 18
+
+// if
+// else
+// elseif
+// let age = 15;
+
+// if(age >= 50){
+//     console.log("You can't vote as well because you are too old");
+//     }else if(age >= 18){
+//         console.log("You can vote");
+//     }else{
+//         console.log("You can't vote");
+//     }
+
+let age = 12;
+let job = true;
+let salary = 500000;
+
+if(age >= 18){
+    if(job){
+        if(salary >= 500000){
+            console.log("You can Married");   
+        }else{
+            console.log("You can't married because of salary issues");    
+        } 
+    }else{
+        console.log("You can't married because have no job");  
+    }  
+}
+else{
+    console.log("You can not married not fullfilled age condition");
+    
+}
+
+
+
+
