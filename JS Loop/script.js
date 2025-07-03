40df906b48402db18c413fcb6bad7da8a945f5f2
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-03 22:07:59 +0600

    Update script.js

--- JS Loop/script.js
+++ JS Loop/script.js
@@ -1,43 +1,14 @@
-// condition
+// Loop (for)
+// startpoint = number 
+// endpoint = number 
 
-// let age = prompt("Please enter your age:");
-
-// sytex if condition if(condition){statement}
-// mathematical way age > 18
-
-// if
-// else
-// elseif
-// let age = 15;
-
-// if(age >= 50){
-//     console.log("You can't vote as well because you are too old");
-//     }else if(age >= 18){
-//         console.log("You can vote");
-//     }else{
-//         console.log("You can't vote");
-//     }
-
-let age = 12;
-let job = true;
-let salary = 500000;
-
-if(age >= 18){
-    if(job){
-        if(salary >= 500000){
-            console.log("You can Married");   
-        }else{
-            console.log("You can't married because of salary issues");    
-        } 
-    }else{
-        console.log("You can't married because have no job");  
-    }  
-}
-else{
-    console.log("You can not married not fullfilled age condition");
+// for loop sytex => for (startpoint; endpoint; specific task){}
+// for(let i=0;i<=10;i++){
+//     console.log(i);
     
+// }
+
+let number = 5;
+for(let i=1; i<=10; i++){
+    console.log(number + " " + " x " + i + " " + "=" + " " + number * i);
 }
-
-
-
-
