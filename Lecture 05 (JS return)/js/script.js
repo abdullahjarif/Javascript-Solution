cef7462ce0e237dac388038f7e483bde65058f7e
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-03 09:17:57 +0600

    Create script.js

---
+++ Lecture 05 (JS return)/js/script.js
@@ -0,0 +1,26 @@
+// function funReturn(a, b) {
+//     console.log(`${a} is first value and ${b} is second value`);
+    
+//     return
+//     console.log(`${a} is first2 value and ${b} is second2 value`);
+    
+// }
+// funReturn(4, 10);
+
+// const result = calculationMath(4,10);
+// function calculationMath(a,b){
+//     return a * b; // if return don't defined so answer is undefined value
+// }
+// // let result = calculationMath(4,10);
+// console.log(result);
+
+function calculationMath(a,b){
+    let result = a * b;
+    return result;  
+}
+let resultAns = calculationMath(4,10)
+console.log(resultAns);
+
+
+
+
