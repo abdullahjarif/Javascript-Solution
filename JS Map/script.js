df6f20dd7b938c280b63bc5f980a5f3a609a7047
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-05 14:03:25 +0600

    Add files via upload

---
+++ JS Map/script.js
@@ -0,0 +1,8 @@
+const student = ["Jarif", "Nadiya", "Arif", "Jasia", "Mayad", "Raj"];
+
+student.map(function(item,index){
+    // console.log(`Index ${index} item ${item}`);
+    // console.log(item);
+    console.log(index, item);
+    
+});
