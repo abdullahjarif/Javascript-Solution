33db25a3ca2e5355c98fffc6b942044793053e17
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-06 21:13:34 +0600

    Add files via upload

---
+++ JS Anonymous Function/script.js
@@ -0,0 +1,18 @@
+// normal function
+// function normal(){
+//     console.log("Normal Function");
+    
+// }
+// normal();
+
+// anonymous function
+// when call anonymous funciton declare with a variable (let)
+let anonymous = function (){
+    console.log("anonymous function");
+    
+}
+anonymous();
+anonymous();
+anonymous();
+anonymous();
+anonymous();
